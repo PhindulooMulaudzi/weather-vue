af0938c64908893614725d15333268fa0b03ab58
Phindulo Mulaudzi <62767186+PhindulooMulaudzi@users.noreply.github.com>
2024-03-21 09:47:32 +0200

    create-react-app

---
+++ client/wather-vue/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <header>
+        <p>Hello, World!</p>
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
